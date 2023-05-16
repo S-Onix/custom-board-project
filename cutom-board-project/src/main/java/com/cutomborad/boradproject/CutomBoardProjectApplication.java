de0830fb1d577c8ca00f00126bc8a033de109ca3
S-Onix <msyoo101@hansung.ac.kr>
2023-05-16 22:06:31 +0900

    #5 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 : 
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ cutom-board-project/src/main/java/com/cutomborad/boradproject/CutomBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cutomborad.boradproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutomBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CutomBoardProjectApplication.class, args);
+    }
+
+}
